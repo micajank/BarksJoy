c67bc5d789c3ff8803e42f2cb88e93f3c8bbd88f
micajank <micajank@umich.edu>
2020-04-17 13:14:49 -0700

    not sure about this, going to pull yours

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import Content from './content/Content'
-//import NavBottom from '../components/NavBottom';
+import { Route } from 'react-router-dom';
+
+import NavBottom from '../components/NavBottom';
 
 
 import { Container, Col, Row } from 'reactstrap';
@@ -10,46 +12,14 @@ import { faUser, faPaw, faComment } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <main>
           <Content />
         </main>
         <div className="nav-bar">
-        <Container>
-                <Row>
-                    <Col>
-                        <div className="nav-button-bar">
-                            <div className="nav-button">
-                                <a href="/"><FontAwesomeIcon color="#d6d6d6" size="2x" icon={faUser} /></a>
-                            </div>
-                            <div className="nav-button">
-                                <a href="/settings"><FontAwesomeIcon color="#d6d6d6" size="2x" icon={faPaw} /></a>
-                            </div>
-                            <div className="nav-button">
-                                <a href="/chat"><FontAwesomeIcon color="#d6d6d6" size="2x" icon={faComment} /></a>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-        </Container>
+          <NavBottom />
         </div>
-
-// import Content from './components/Content'
-// import NavBottom from './components/NavBottom';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <main>
-//         <Content />
-//       </main>
-//       <div className="nav-bar">
-//         <NavBottom />
-
       </div>
-    </div>
-  );
-}
+  )}
 
 export default App;
